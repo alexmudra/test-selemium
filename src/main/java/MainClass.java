f49e205be585ddb52fafd911eb6c0a34509ebf8d
alexmudra <alexander.kobko@gmail.com>
2018-04-02 23:16:39 +0300

    Operations with checkboxes and radiobuttons

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -21,9 +21,8 @@ public class MainClass {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         /*методы для управления размером окна браузера*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-        driver.manage().window().maximize(); // метод открывает в максимальном размере окно браузера
+        //driver.manage().window().maximize(); // метод открывает в максимальном размере окно браузера
+        driver.manage().window().setSize(new Dimension(1330, 900));
 
         /*РАБОТА С ТЕКСТОВЫМИ ПОЛЯМИ. КАК ЗАПОЛНЯТЬ И ОТОБРАЖАТЬ ТЕКСТ В ТЕКСТОВЫХ ПОЛЯХ*/
 
@@ -45,6 +44,7 @@ public class MainClass {
 
         /*РАБОТА СО ССЫЛКАМИ*/
 
+        /*
         driver.get("https://ru.wikipedia.org");//открываем сайт википедии
         WebElement link = driver.findElement(By.xpath("//*[@id=\"p-wikibase-otherprojects\"]/div/ul/li[1]/a"));// присваемаем переменной link
         //значение длайвера ссылки
@@ -56,22 +56,34 @@ public class MainClass {
 
         driver.close();// закрить попап вікно
         driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
-=======
-        driver.manage().window().maximize();// метод открывает в максимальном размере окно браузера
+        */
 
         /*РАБОТА С КНОПКАМИ*/
 
-        driver.get("https://ru.wikipedia.org");//открываем сайт википедии
+        /*driver.get("https://ru.wikipedia.org");//открываем сайт википедии
+        driver.findElement(By.xpath("//input[@id='searchButton']")).click();//находим елемент поиск и кликаем
+
+        driver.get("https://www.spreadshirt.co.uk");//открываем сайт
+        driver.findElement(By.xpath("//*[@id='basketButton']/div[1]")).click();//кликаем по елементу корзина
+
+        //driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
+
+        driver.manage().window().maximize();// метод открывает в максимальном размере окно браузера*/
+
+        /*РАБОТА С КНОПКАМИ*/
+
+        /*driver.get("https://ru.wikipedia.org");//открываем сайт википедии
         driver.findElement(By.xpath("//input[@id='searchButton']")).click();//находим елемент поиск и кликаем
 
         driver.get("https://www.spreadshirt.co.uk");//открываем сайт
         driver.findElement(By.xpath("//*[@id='basketButton']/div[1]")).click();//кликаем по елементу корзина
 
         driver.get("https://github.com");// открыли сайт гитхаб
-        WebElement button = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button"));// присвоили вебелементу класса вебелемент значение
+        WebElement button2 = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button"));// присвоили
+        // вебелементу класса вебелемент значение
         //кнопки
-            if (button.getText().equals("Sign up for GitHub")){
-                System.out.println("Button test is " + button.getText());
+            if (button2.getText().equals("Sign up for GitHub")){
+                System.out.println("Button test is " + button2.getText());
         }
             else System.out.println("The btn text is not correct");
 
@@ -79,30 +91,30 @@ public class MainClass {
         driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button")).click();
 
         //driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
->>>>>>> 655f0c4... Intro to operate Buttons
-=======
-        driver.manage().window().maximize();// метод открывает в максимальном размере окно браузера
+        */
 
-        /*РАБОТА С КНОПКАМИ*/
+        /*РАБОТА С РАДИОБАТТОНАМИ И ЧЕКБОКСАМИ*/
 
-        driver.get("https://ru.wikipedia.org");//открываем сайт википедии
-        driver.findElement(By.xpath("//input[@id='searchButton']")).click();//находим елемент поиск и кликаем
+        driver.get("https://rozetka.com.ua");// открыли сайт прайс юа
 
-        driver.get("https://www.spreadshirt.co.uk");//открываем сайт
-        driver.findElement(By.xpath("//*[@id='basketButton']/div[1]")).click();//кликаем по елементу корзина
+        driver.findElement(By.xpath("//*[@id=\"2416\"]/a")).click();// открыли подкатегорию Ноутбуки и компьютеры
+        driver.findElement(By.xpath("//*[@id=\"content-inner-block\"]/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div[3]/div/div/div[1]/div/div[1]/div[1]/div/p/a")).click(); //Ноутбуки
+        driver.findElement(By.xpath("//*[@id=\"menu_categories_left\"]/li[2]/ul/li[1]/a")).click();// открываем категорию Асер
+        driver.findElement(By.xpath("//*[@id=\"filter_processor_23653\"]/label/a/span/i")).click();// через метод клик ставим галочку на AMDA4
 
-        driver.get("https://github.com");// открыли сайт гитхаб
-        WebElement button = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button"));// присвоили вебелементу класса вебелемент значение
-        //кнопки
-            if (button.getText().equals("Sign up for GitHub")){
-                System.out.println("Button test is " + button.getText());
-        }
-            else System.out.println("The btn text is not correct");
+        //Проексперементируем с методом isSelected и условиями
+        if (!driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected());/*если isSelected
+        вернет фолс, то мы кликнем на чекбокс. "!driver..." - мы проверили отрицание поставив "!"*/
+        driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).click();//это мы кликаем на чекбокс если он не выделен
 
-        //button.submit();//вызываем метод submit для елемента button
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button")).click();
+        /*Проверяем выделеи или не выделен елемент с помощью метода .isSelected (метод возвражает булеан значиение)
+        * Если мы применили етот метод к чекбоксу то false значит что чекбокс НЕ выделен
+        * */
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected()); //проверяем тру или фолс
+        driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).click();// ставим галочку на етом же чекбоксе
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected()); //проверяем тру или
 
-        //driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
->>>>>>> 655f0c4... Intro to operate Buttons
+        driver.close();// закрить попап вікно
+        driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
     }
 }
