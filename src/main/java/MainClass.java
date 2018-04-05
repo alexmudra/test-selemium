964de9c12094e53332a010ceb1e0c72c571c3968
alexmudra <alexander.kobko@gmail.com>
2018-04-06 00:38:27 +0300

    Operations with drop-dowl lists and elements. Create methods

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -12,11 +12,15 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by Alex on 2/25/2018.
  */
+
+
 public class MainClass {
 
     static WebDriver driver; // для универсальности доступа вынесли драйвер за пределы метода
 
+
     public static void main(String[] args) {// создали метод чтобы инициализировать драйвер GecoDriver для FF
+
         System.setProperty("webdriver.gecko.driver", "D:\\QA\\testselenium\\drivers\\geckodriver.exe");// указали путь к драйверу
         driver = new FirefoxDriver();// инициализировали наш драйвер (FF драйвер)
 
@@ -27,104 +31,50 @@ public class MainClass {
         //driver.manage().window().maximize(); // метод открывает в максимальном размере окно браузера
         driver.manage().window().setSize(new Dimension(1330, 800));
 
-        /*РАБОТА С ТЕКСТОВЫМИ ПОЛЯМИ. КАК ЗАПОЛНЯТЬ И ОТОБРАЖАТЬ ТЕКСТ В ТЕКСТОВЫХ ПОЛЯХ*/
 
-        /*driver.get("https://ru.wikipedia.org");//открываем сайт википедии
-        driver.findElement(By.xpath("//*[@id=\"searchInput\"]")).sendKeys("car");// method .sendKeys вставляе текст в текстове поле
-        driver.findElement(By.xpath("//*[@id=\"searchButton\"]")).click();//находим елемент поиск и кликаем
-        driver.findElement(By.xpath("//*[@id=\"searchButton\"]")).click();//находим елемент поиск и кликаем
-        System.out.println(driver.findElement(By.xpath("//*[@id=\"searchText\"]")).getAttribute("value"));
-        driver.findElement(By.xpath("//*[@id=\"ooui-php-1\"]")).clear();// .clear очищает поле ввода*/
+        //РАБОТА С ЛИСТ-БОКСАМИ ИЛИ ВЫПАДАЮЩИМИ СПИСКАМИ
 
-        // Попробуем передать значения в другой ресурс (Гитхаб)
+        //открываем форму регистрации на гугл
+        driver.get("https://accounts.google.com/SignUp?continue=https%3A%2F%2Fwww.google.com.ua%2F_%2Fchrome%2Fnewtab%3Fei%3DWGHGWvfsCcfv6QSllafIAw%26yv%3D2&hl=ru");
 
-        /*driver.get("https://github.com/");// открываем сайт
-        driver.findElement(By.xpath("//*[@id=\"user[login]\"]")).sendKeys("Test User Name"); //находим елемент и указываем юзернейм
-        driver.findElement(By.xpath("//*[@id=\"user[email]\"]")).sendKeys("testEmail@gmail.com");// находим и указываем емайл
-        driver.findElement(By.xpath("//*[@id=\"user[password]\"]")).sendKeys("3837847");
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button")).submit();
-        */
+        //Вызовем новый метод selectOption и передадим имена и значения листбоксов. Метод запустит и выберет то что нужно со списков
 
-        /*РАБОТА СО ССЫЛКАМИ*/
+        selectOption("Пол", "Мужской");
+        selectOption("Страна","Италия (Italia)");
 
-        /*
-        driver.get("https://ru.wikipedia.org");//открываем сайт википедии
-        WebElement link = driver.findElement(By.xpath("//*[@id=\"p-wikibase-otherprojects\"]/div/ul/li[1]/a"));// присваемаем переменной link
-        //значение длайвера ссылки
-        System.out.println(link.getText());// выводим на екран текст ссылки переменной link
-        link.click();//методом клік клікаємо по лінку
 
-        driver.get("https://github.com");//відкрити сайт гітхаба
-        driver.findElement(By.xpath("/html/body/div[1]/header/div/div[2]/nav/ul/li[1]/a")).click();//знаходимо лінк по ікспасу і клікаємо
+     /*   driver.findElement(By.xpath("(//strong[text()='Пол']/following-sibling::div/div[@role='listbox'])[1]")).click();//нашли и кликнули на листбокс с Пол
+        driver.findElement(By.xpath("(//div[text()='Мужской']/parent::div[@role='option'])")).click();//кликаем по опции "Мужской" в листбоксе ПОЛ
 
-        driver.close();// закрить попап вікно
-        driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
-        */
+        //выберем еще значения с листбокса Страна
+        driver.findElement(By.xpath("(//strong[text()='Страна']/following-sibling::div/div[@role='listbox'])[1]")).click();//нашли и кликнули на листбокс с Страна
+        driver.findElement(By.xpath("(//div[text()='Аруба (Aruba)']/parent::div[@role='option'])")).click();//кликаем по опции "Аруба" в листбоксе Страна
+    */
+        //Напишем отдельный метод для выбора листбоксов и значений в листбоксах
 
-        /*РАБОТА С КНОПКАМИ*/
-
-        /*driver.get("https://ru.wikipedia.org");//открываем сайт википедии
-        driver.findElement(By.xpath("//input[@id='searchButton']")).click();//находим елемент поиск и кликаем
-
-        driver.get("https://www.spreadshirt.co.uk");//открываем сайт
-        driver.findElement(By.xpath("//*[@id='basketButton']/div[1]")).click();//кликаем по елементу корзина
-
+        //driver.close();// закрить попап вікно
         //driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
+    }
+        //Поскольку у нас много повторяющегося кода для чекбоксов и радиботонов-мы напишем отдельный метод для их обработки
 
-        driver.manage().window().maximize();// метод открывает в максимальном размере окно браузера*/
+        //Напишем отдельный метод для выбора листбоксов и значений в листбоксах
+    public static void selectOption(String listName, String option ) { //создали метод который будет принимать 2 стринговых значения
 
-        /*РАБОТА С КНОПКАМИ*/
+        //сосдадим 2 локатора чтобы не привязиваться к жестким условиям метода
 
-        /*driver.get("https://ru.wikipedia.org");//открываем сайт википедии
-        driver.findElement(By.xpath("//input[@id='searchButton']")).click();//находим елемент поиск и кликаем
+        //мы заменили опцию Страна на '%s' и использовали метод .format и присвоили значение переменной listName
+        String listXpath = String.format("(//strong[text()='%s']/following-sibling::div/div[@role='listbox'])[1]",listName );
 
-        driver.get("https://www.spreadshirt.co.uk");//открываем сайт
-        driver.findElement(By.xpath("//*[@id='basketButton']/div[1]")).click();//кликаем по елементу корзина
+        //вместо опции Аруба (Aruba) мы заменили на '$s'использовали метод .format и присвоили значение переменной option
+        String optionXpath = String.format("(//div[text()='%s']/parent::div[@role='option'])", option);
 
-        driver.get("https://github.com");// открыли сайт гитхаб
-        WebElement button2 = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button"));// присвоили
-        // вебелементу класса вебелемент значение
-        //кнопки
-            if (button2.getText().equals("Sign up for GitHub")){
-                System.out.println("Button test is " + button2.getText());
-        }
-            else System.out.println("The btn text is not correct");
+        //используем переменные/локаторы listName and option в методе findElement
+        driver.findElement(By.xpath(listXpath)).click();//передаем новый икспас listXpath в метод
+        driver.findElement(By.xpath(optionXpath)).click();//передаем новый икспас optionXpath в метод
 
-        //button.submit();//вызываем метод submit для елемента button
-        driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div/div[2]/div/form/button")).click();
-
-        //driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
-        */
-
-        /*РАБОТА С РАДИОБАТТОНАМИ И ЧЕКБОКСАМИ*/
-
-        driver.get("https://rozetka.com.ua");// открыли сайт прайс юа
-
-        driver.findElement(By.xpath("//*[@id=\"2416\"]/a")).click();// открыли подкатегорию Ноутбуки и компьютеры
-        driver.findElement(By.xpath("//*[@id=\"content-inner-block\"]/div[2]/div/div/div[2]/div/div[1]/div[1]/div/div[3]/div/div/div[1]/div/div[1]/div[1]/div/p/a")).click(); //Ноутбуки
-        driver.findElement(By.xpath("//*[@id=\"menu_categories_left\"]/li[2]/ul/li[1]/a")).click();// открываем категорию Асер
-        driver.findElement(By.xpath("//*[@id=\"filter_processor_23653\"]/label/a/span/i")).click();// через метод клик ставим галочку на AMDA4
-
-        //Проексперементируем с методом isSelected и условиями
-        if (!driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected());/*если isSelected
-        вернет фолс, то мы кликнем на чекбокс. "!driver..." - мы проверили отрицание поставив "!"*/
-        driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).click();//это мы кликаем на чекбокс если он не выделен
-
-        /*Проверяем выделеи или не выделен елемент с помощью метода .isSelected (метод возвражает булеан значиение)
-        * Если мы применили етот метод к чекбоксу то false значит что чекбокс НЕ выделен
-        * */
-        System.out.println(driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected()); //проверяем тру или фолс
-        driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).click();// ставим галочку на етом же чекбоксе
-        System.out.println(driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected()); //выводим на екран значение тру или фолс
 
         driver.close();// закрить попап вікно
         driver.quit();// обязательно указывать вконце теста даный метод для завершения работы драйвера
     }
-        //Поскольку у нас много повторяющегося кода для чекбоксов и радиботонов-мы напишем отдельный метод для них
 
-    public static void selectCheckBox (String name){//создаем отдельный метод (код метода нужно доработать)
-        String rbXpath = "//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]";
-        if (!driver.findElement(By.xpath("//*[@id=\"filter_processor_13288\"]/label/a/span/i[1]")).isSelected());;//делаем условие проверку выделен или не выделен радиобатон
-            driver.findElement(By.xpath(String.format(rbXpath, name))).click();//драйвер будет кликать если икспас содержит в себе имя(код нужно доработать)
-    }
 }
