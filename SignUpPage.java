31ec8160dae2135c9d490e6aec6fc689f3a1793e
alexmudra <alexander.kobko@gmail.com>
2018-05-29 10:05:48 +0300

    describe Sing up page for GIT HUB

--- SignUpPage.java
+++ SignUpPage.java
@@ -2,7 +2,10 @@
 
 //СТВОРИМО КЛАС ДЛЯ СТОРІНКИ SING UP PAGE
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+//ОПИШЕМ КЛАС SING UP ДЛЯ ГІТХАБУ
 
 public class SignUpPage {
 
@@ -11,4 +14,15 @@ public class SignUpPage {
     public SignUpPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    private By heading = By.xpath("//div[@class=\"setup-wrapper\"]//h1");//локатор для надпису в хедері
+    private By userEmail = By.xpath("//input[@class=\"form-control\"]");//локатор для поля емайл
+    private By singUpButton = By.xpath("//button[@class=\"btn btn-primary\"]\n");//локатор для кнопки
+
+
+
+
+
+
+
 }
